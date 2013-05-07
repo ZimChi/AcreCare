0bc0f0c489f2450ba3c946bad13411e429953170
Chielo Zimmerman <chielo.zimmerman@gmail.com>
2013-05-06 20:49:56 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AcreCare::Application.config.secret_token = 'd8adf6cf66ad70e6e6575617502828b2c57d91e0fa8023183bde48f73970d5291a8d640608a0da4774d81cd8a3d692086ad346c7619be357a3cb938143a8157c'
