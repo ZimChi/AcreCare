a279be35f0335f302e14d7642660decf325b63b3
Chielo Zimmerman <chielo.zimmerman@gmail.com>
2013-05-09 16:30:23 -0400

    minor route repair

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ AcreCare::Application.routes.draw do
 
   root :to => "parcels#index"
 
-  match "/thanks" => "thanks#index"
-  match "/certificate" => "thanks#download"
+  match "/thanks" => "pages#index"
+  match "/certificate" => "pages#download"
   match "/about" =>"pages#about"
 
 end
